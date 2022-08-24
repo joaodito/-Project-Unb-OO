0854c106a1dc7af91da74f6352f68821b4482081
João Costa <58365406+joaodito@users.noreply.github.com>
2022-08-24 16:07:37 -0300

    Update Combustivel Class
    Include attributes of Combustivel

--- Dicob/src/model/Combustivel.java
+++ Dicob/src/model/Combustivel.java
@@ -2,5 +2,30 @@ package model;
 
 public class Combustivel {
 	
+	private double precoCombustivel;
+	private String tipoDeCombustivel;
+
+	public Combustivel(double precoCombustivel, String tipoDeCombustivel){
+		this.precoCombustivel = precoCombustivel;
+		this.tipoDeCombustivel = tipoDeCombustivel;
+	}
+
+	public double getPrecoCombustivel() {
+		return precoCombustivel;
+	}
+
+	public void setPrecoCombustivel(double precoCombustivel) {
+		this.precoCombustivel = precoCombustivel;
+	}
+
+	public String getTipoDeCombustivel() {
+		return tipoDeCombustivel;
+	}
+
+	public void setTipoDeCombustivel(String tipoDeCombustivel) {
+		this.tipoDeCombustivel = tipoDeCombustivel;
+	}
 	
 }
+
+
